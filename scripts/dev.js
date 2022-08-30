081669eb59843a2c83c95d98e0d6fd7c8792e52b
启煜 <chenyu.fcy@antgroup.com>
2022-08-30 18:15:18 +0800

    feat: 增加dev脚本对macOS平台的支持

--- scripts/dev.js
+++ scripts/dev.js
@@ -2,9 +2,17 @@ const path = require('path');
 const chalk = require('chalk');
 const cp = require('child_process');
 
+const { WEBPACK_COMMAND, ELECTRON_COMMAND } = process.platform === 'win32' ? {
+	WEBPACK_COMMAND: 'webpack.cmd',
+	ELECTRON_COMMAND: 'electron.cmd'
+} : {
+	WEBPACK_COMMAND: 'webpack',
+	ELECTRON_COMMAND: 'electron'
+}
+
 const DIRNAME = path.resolve(__dirname, '..');
 // 1. 启动渲染进程
-const react = cp.spawn('webpack.cmd', ['serve', '--mode', 'development', '--env', 'development'], {
+const react = cp.spawn(WEBPACK_COMMAND, ['serve', '--mode', 'development', '--env', 'development'], {
 	cwd: DIRNAME,
 });
 react.unref();
@@ -12,7 +20,7 @@ react.unref();
 // 2. 启动 electron
 cp.execSync('tsc', { cwd: path.resolve(DIRNAME, 'src', 'main') });
 const { DEV_PORT, APP_ICON } = require('../config/dev.config');
-const electron = cp.spawn('electron.cmd', ['.', `--port=${DEV_PORT}`, `--icon=${APP_ICON}`], {
+const electron = cp.spawn(ELECTRON_COMMAND, ['.', `--port=${DEV_PORT}`, `--icon=${APP_ICON}`], {
 	cwd: DIRNAME,
 });
 electron.on('close', () => {
@@ -20,9 +28,10 @@ electron.on('close', () => {
 		case 'win32':
 			cp.exec(`taskkill /pid ${react.pid} /T /F`);
 			break;
-		// TODO: 完善其他平台下方法
 		case 'darwin':
+			cp.exec(`kill -9 ${react.pid}`);
 			break;
+		// TODO: 完善其他平台下方法
 		case 'linux':
 			break;
 	}
